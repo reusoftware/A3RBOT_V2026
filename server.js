a1d238c85330b030a9707bfdcb904c6d75327b85
reusoftware <105401707+reusoftware@users.noreply.github.com>
2026-05-03 21:13:55 +0800

    server.js

--- server.js
+++ server.js
@@ -1,18 +1,11 @@
 const express = require("express");
 const app = express();
 
-const PORT = process.env.PORT;
-
 app.get("/", (req, res) => {
     res.send("✅ A3R BOT IS LIVE ON RAILWAY");
 });
 
-const PORT = process.env.PORT;
-
-if (!PORT) {
-    console.error("PORT not provided by Railway!");
-    process.exit(1);
-}
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
     console.log("A3R BOT RUNNING ON PORT " + PORT);
