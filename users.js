c8dd308170ffd20f87ef1153df5ee9df07e3c522
reusoftware <105401707+reusoftware@users.noreply.github.com>
2026-05-03 20:37:57 +0800

    users.js

---
+++ users.js
@@ -0,0 +1,13 @@
+
+exports.users = [
+    {
+        username: "admin",
+        password: "1234",
+        role: "master"
+    },
+    {
+        username: "botuser",
+        password: "1111",
+        role: "user"
+    }
+];
