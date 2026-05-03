b6b4daa60e87ca09434a1841ef67b888b697d40d
reusoftware <105401707+reusoftware@users.noreply.github.com>
2026-05-03 21:00:18 +0800

    server.js

--- server.js
+++ server.js
@@ -98,6 +98,8 @@ app.get("/dashboard", (req, res) => {
     res.sendFile(__dirname + "/public/dashboard.html");
 });
 
-app.listen(process.env.PORT || 8080, () => {
-    console.log("A3R BOT RUNNING");
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, "0.0.0.0", () => {
+    console.log("🤖 A3R BOT RUNNING ON PORT " + PORT);
 });
