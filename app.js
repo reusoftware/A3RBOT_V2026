027e2c6fe453b92a45ef3a050ac7d350650dbba8
reusoftware <105401707+reusoftware@users.noreply.github.com>
2026-05-15 05:55:47 +0800

    app.js

--- app.js
+++ app.js
@@ -8,13 +8,3 @@ async function boot() {
         process.env.BOT_USERNAME,
         process.env.BOT_PASSWORD
     );
-
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-
-    console.log(
-        "Server running on port " + PORT
-    );
-
-});
